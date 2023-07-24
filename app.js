6c7145aae6ee71e6bf7ac9581f8534e518344d77
Savadow <ujuchimaraoke@gmail.com>
2023-07-24 18:21:58 +0100

    feat: setup database connection

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+// app.use("/api/v1/account")
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        console.log("Server is listening on port 3000...");
+    } catch (error) {
+        console.log(error);
+    }
+}
